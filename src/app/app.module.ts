932f0d6b8ab220398ce34cfe6a22006ca86419ee
DevLomoSE <jonathancggs@gmail.com>
2021-09-03 10:07:59 -0500

    :recycle: Imports
    refactor app.module.ts to use the new imports module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ImportsModule } from './imports.module';
+import { ImportsModule } from './imports/imports.module';
 
 @NgModule({
   declarations: [
